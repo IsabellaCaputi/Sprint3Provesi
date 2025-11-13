0bb6cca2f2819f571094b8f424a6b462050996b8
IsabellaCaputi <110950027+IsabellaCaputi@users.noreply.github.com>
2025-11-12 21:10:36 -0500

    intento 4

--- auth/deployment.tf
+++ auth/deployment.tf
@@ -167,29 +167,6 @@ resource "aws_instance" "monitoring" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.traffic_django.id, aws_security_group.traffic_ssh.id]
 
-  user_data = <<-EOT
-              #!/bin/bash
-
-              sudo export DATABASE_HOST=${aws_instance.database.private_ip}
-              echo "DATABASE_HOST=${aws_instance.database.private_ip}" | sudo tee -a /etc/environment
-
-              sudo apt-get update -y
-              sudo apt-get install -y python3-pip python3-venv git build-essential libpq-dev python3-dev
-
-              sudo mkdir -p /labs
-              cd /labs
-
-              if [ ! -d DesignStormers-MonitoringApp-Auth0 ]; then
-                git clone ${local.repository}
-              fi
-
-              cd DesignStormers-MonitoringApp-Auth0
-              sudo pip3 install --upgrade pip --break-system-packages
-              sudo pip3 install -r requirements.txt --break-system-packages
-
-              sudo python3 manage.py makemigrations
-              sudo python3 manage.py migrate
-              EOT
 
   tags = merge(local.common_tags, {
     Name = "${var.project_prefix}-django"
